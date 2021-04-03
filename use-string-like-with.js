42726452431c49f2f12378e8d4b768fb69b30068
VSADX <81491645+VSADX@users.noreply.github.com>
2021-04-03 01:37:56 -0500

    Create use-string-like-with.js

---
+++ use-string-like-with.js
@@ -0,0 +1,38 @@
+// from https://gist.github.com/0kku/d67e9a685bde9c159ff3bc4c4ee923b1
+// by   https://github.com/0kku
+function use (
+  ...objects
+) {
+  const values = [];
+  const keys = (
+    objects
+    .flatMap(object => {
+      const prototypeChain = [];
+      do {
+        prototypeChain.unshift(object);
+      } while (object = Object.getPrototypeOf(object));
+      return prototypeChain;
+    })
+    .flatMap(object => {
+      const keys = Object.getOwnPropertyNames(object);
+      values.push(...keys.map(key => object[key]));
+      return keys;
+    })
+  );
+
+  return (
+    strings,
+    ...slots
+  ) => new Function(
+    ...keys,
+    strings.reduce((a, b, i) => a + String(slots[i]) + b),
+  )(...values);
+}
+
+console.log(
+  With(Math, {a: 2})`
+    return valueOf.call(PI * a);
+  `
+);
+
+export {};
